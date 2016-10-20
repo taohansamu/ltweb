0a7baffd85701b80b6d6d47ff017567868020e94
taohansamu <dinhtao995@gmail.com>
2016-10-20 22:54:14 +0700

    change server for hosting

--- module/homework6/connectdatabase.php
+++ module/homework6/connectdatabase.php
@@ -7,10 +7,10 @@ $connect = mysql_connect($server,$user,$pass,$db);
 if (!$connect) {
 	die("Cannot connect to Database Server");
 } */
-$server = 'localhost';
-$user = 'root';
-$pass = '';
-$db='shoppingcart';
+$server = 'mysql.hostinger.vn';
+$user = 'u685691000_root';
+$pass = 'taohansamu995';
+$db='u685691000_spc';
 $connect = mysqli_connect($server,$user,$pass,$db);
 if (!$connect) {
 	die("Cannot connect to Database Server");
